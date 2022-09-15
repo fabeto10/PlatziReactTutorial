87401deef5f615c2fab963206bfb5d11c12b4e67
fabeto10 <f0414475@gmail.com>
2022-09-15 11:15:13 -0400

    se agrega TodoContext y useLocalStorage como componente

--- src/App/index.js
+++ src/App/index.js
@@ -9,39 +9,60 @@ import { AppUI } from "./AppUI";
 // ];
 
 function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+  const [loading, setLoading] = React.useState(true)
+  const [item, setItem] = React.useState(initialValue);
+  const [error, setError] =React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(()=>{
+      try{
+          const localStorageItem = localStorage.getItem(itemName);
+          let parsedItem;
+          
+          if (!localStorageItem) {
+            localStorage.setItem(itemName, JSON.stringify(initialValue));
+            parsedItem = initialValue;
+          } else {
+            parsedItem = JSON.parse(localStorageItem);
+          }
+          setItem(parsedItem);
+          setLoading(false);
+        
+      } catch(error){
+        setError(error);
+      }
+    }, 1000);
+  });
   
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-
-  const [item, setItem] = React.useState(parsedItem);
-
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
+    try{
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error){
+      setError(error);
+    }
   };
 
-  return [
+  return {
     item,
-    saveItem
-  ];
+    saveItem,
+    loading,
+    error,
+  }
 }
 
 function App() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
 
   const [searchValue, setSearchValue] = React.useState('');
-
   const completedTodos = todos.filter(todo => !!todo.completed ).length;
-
   const totalTodos = todos.length;
-
   let searchedTodos = [];
 
   if(!searchValue.length >= 1) {
@@ -83,6 +104,8 @@ function App() {
 
   return (
     <AppUI
+      loading={loading}
+      error={error}
       total={totalTodos}
       completed={completedTodos}
       searchValue={searchValue}
